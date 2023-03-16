7cf2d67b4bc60062a0714f518aa0e0a509fc23a8
bdas615 <bdas@brandinstitute.com>
2023-03-16 20:58:40 +0530

    complete library management system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,57 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LibraryComponent } from './library/library.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { OrderComponent } from './order/order.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ReturnBookComponent } from './return-book/return-book.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { ManageBooksComponent } from './manage-books/manage-books.component';
+import { ManageCategoriesComponent } from './manage-categories/manage-categories.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageFooterComponent,
+    PageHeaderComponent,
+    SideNavComponent,
+    LibraryComponent,
+    RegisterComponent,
+    LoginComponent,
+    OrderComponent,
+    OrdersComponent,
+    ReturnBookComponent,
+    UsersListComponent,
+    ManageBooksComponent,
+    ManageCategoriesComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config:{
+        tokenGetter:()=>{
+          return localStorage.getItem('access_token');
+        },
+        allowedDomains : ['localhost:7253'],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
